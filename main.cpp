9b43cafad4421b56d7d4f1b213e33a2506ae9e5d
zej72 <hogzej@proton.me>
2026-07-06 01:39:40 +0200

    average out fps for readability

--- main.cpp
+++ main.cpp
@@ -21,7 +21,10 @@ int main()
     //rayTracer.scene.objects.push_back( new Sphere({0,0,0}, 1));
     rayTracer.scene.objects.push_back( new Ring({0,0,0}, {1,4,0}, 2, 3));
 
-
+	// only for average fps. not required	
+	double last_render_times[100] = {0};
+	double average_render_time;
+	
     // setup light source
     rayTracer.sun = {10, 5, 0};
 
@@ -34,9 +37,16 @@ int main()
         // top text
         rayTracer.render();
 
+		last_render_times[rayTracer.frame_count%100] = rayTracer.render_time;
+		average_render_time = 0;
+		for (int i = 0; i < 100; i++ ){
+			average_render_time += last_render_times[i];
+		}
+		average_render_time/=100;
+		
         // telemetry
-        cout << "\033[1mfps: " << 1000/(rayTracer.render_time);
-        cout << " / render time: " << rayTracer.render_time << "ms\033[0m";
+        cout << "\033[1mfps: " << (int) (1000/average_render_time);
+        cout << " / render time: " << average_render_time << "ms\033[0m";
         cout << "\n frame: " << rayTracer.frame_count;
     }
     return 0;
